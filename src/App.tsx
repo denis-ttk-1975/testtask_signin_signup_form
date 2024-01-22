f9f68183a15dac8fc0cfdb553e592242adef69dc
denis-ttk-1975 <denis.ttk@gmail.com>
2024-01-23 01:16:53 +0300

    initial elements done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './app.module.css';
+
+import { ArtBoard } from './components/art-board/art-board';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main className={styles.main}>
+        <span>Your Logo</span>
+        <ArtBoard />
+      </main>
+    </>
   );
 }
 
---
+++ src/components/art-board/art-board.tsx
@@ -0,0 +1,8 @@
+import { JSX } from 'react'; // импорт библиотеки
+
+import styles from './art-board.module.css';
+
+export const ArtBoard = () => {
+  return <div className={styles.artBoard}>АРТБОРД</div>;
+}
+
